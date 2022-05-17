5dafb7a1bb24612f4b127bdd3c3c231ce98b4bba
nawar33 <nawarmaaz33@gmail.com>
2022-05-17 07:35:13 -0700

    nawar

---
+++ app/Policies/PharmacyPolicy.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\User;
+use Illuminate\Auth\Access\HandlesAuthorization;
+use Illuminate\Auth\Access\Response;
+
+class PharmacyPolicy
+{
+    use HandlesAuthorization;
+
+    /**
+     * Determine whether the user can view any models.
+     *
+     * @param User $user
+     * @return Response|bool
+     */
+    public function viewAny(User $user): Response|bool
+    {
+        //
+        return ($user->hasRole('Admin'));
+    }
+
+    /**
+     * Determine whether the user can view the model.
+     *
+     * @param User $user
+     * @param User $model
+     * @return Response|bool
+     */
+    public function view(User $user, User $model): Response|bool
+    {
+        return true;
+    }
+
+    /**
+     * Determine whether the user can create models.
+     *
+     * @param User $user
+     * @return Response|bool
+     */
+    public function create(User $user): Response|bool
+    {
+        //
+        return $user->hasRole("Admin");
+    }
+
+    /**
+     * Determine whether the user can update the model.
+     *
+     * @param User $user
+     * @param User $model
+     * @return Response|bool
+     */
+    public function update(User $user, User $model): Response|bool
+    {
+        //
+        return ($user->id == $model->id);
+    }
+
+    /**
+     * Determine whether the user can delete the model.
+     *
+     * @param User $user
+     * @param User $model
+     * @return Response|bool
+     */
+    public function delete(User $user, User $model)
+    {
+        if ($user->hasRole('Admin')) {
+            return true;
+        }
+        return ($user->id == $model->id);
+
+    }
+
+    /**
+     * Determine whether the user can restore the model.
+     *
+     * @param User $user
+     * @param User $model
+     * @return Response|bool
+     */
+    public function restore(User $user, User $model)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can permanently delete the model.
+     *
+     * @param User $user
+     * @param User $model
+     * @return Response|bool
+     */
+    public function forceDelete(User $user, User $model)
+    {
+        //
+    }
+}
